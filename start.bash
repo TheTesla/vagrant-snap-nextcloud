786a077969fe1a14a9f9652cff148d74aba35ea4
Stefan <stefan.helmert@t-online.de>
2018-08-06 01:08:39 +0200

    fix: storing all virtualbox data locally

--- start.bash
+++ start.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-export VAGRANT_HOME=./vagranthome
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+export VAGRANT_HOME=$DIR/vagranthome
+export VBOX_USER_HOME=$DIR/vbox
+VBoxManage setproperty machinefolder $VBOX_USER_HOME
 vagrant plugin install vagrant-disksize
+#vagrant plugin install vagrant-libvirt
 vagrant up
+#VBoxManage setproperty machinefolder default
 
